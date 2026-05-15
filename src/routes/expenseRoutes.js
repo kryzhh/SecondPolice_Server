8b51b68ee0011c2dcbd6d0728097761231be4a02
Nitin <sales@traincapetech.in>
2026-05-15 09:14:15 +0000

    feat: added expense section in hr

--- src/routes/expenseRoutes.js
+++ src/routes/expenseRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const expenseController = require('../controllers/expenseController');
 const { authenticate } = require('../middlewares/authMiddleware');
-const { requirePermission } = require('../middlewares/rbacMiddleware');
+
 
 const router = express.Router();
 
 router.use(authenticate);
 
-router.get('/', requirePermission('Expenses', 'Read-Only'), expenseController.getExpenses);
-router.get('/:id', requirePermission('Expenses', 'Read-Only'), expenseController.getExpense);
-router.post('/', requirePermission('Expenses', 'Read & Write'), expenseController.createExpense);
-router.patch('/:id', requirePermission('Expenses', 'Read & Write'), expenseController.updateExpense);
-router.delete('/:id', requirePermission('Expenses', 'Read & Write'), expenseController.deleteExpense);
+router.get('/', expenseController.getExpenses);
+router.get('/:id', expenseController.getExpense);
+router.post('/', expenseController.createExpense);
+router.patch('/:id', expenseController.updateExpense);
+router.delete('/:id', expenseController.deleteExpense);
 
 module.exports = router;
